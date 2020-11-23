6a89e02d9a22b37fc9188e88b0d3759ab47d94b7
Lawton255 <elawton255@gmail.com>
2020-11-24 01:49:59 +0300

    Search view and filter view implemented

--- models/student.py
+++ models/student.py
@@ -4,9 +4,11 @@ from odoo import models, fields
 
 class StudentStudent(models.Model):
 	_name = 'student.student'
+	#_inherit = 'mail.thread'
+	_description = 'Student Recors'
 
-	name = fields.Char(string='Name', required=True)
-	age = fields.Integer(string='Age')
+	name = fields.Char(string='Name', required=True, track_visibility='always')
+	age = fields.Integer(string='Age' , track_visibility='always')
 	photo = fields.Binary(string='Image')
 	gender = fields.Selection([('male', 'Male'), ('female', 'Female'), ('others', 'Others')], string='Gender')
 	student_dob = fields.Date(string="Date of Birth")
